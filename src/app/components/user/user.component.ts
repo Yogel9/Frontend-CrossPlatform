5dec6c15fbdbc4acfbe65ac3cc269345677f3cd7
Yogel9 <Dovidenkov@users.noreply.github.com>
2021-06-04 21:08:25 +0300

    First array working

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,20 +8,24 @@ export class User{
 
 @Component({
   selector: 'app-user',
-  template: `<div>
-    <p>Имя пользователя: {{user?.name}}</p>
-    <p>Возраст пользователя: {{user?.age}}</p>
-  </div>`,
+  template: `<ul>
+    <li *ngFor="let user of users">
+      <p>Имя пользователя: {{user?.name}}</p>
+      <p>Возраст пользователя: {{user?.age}}</p>
+    </li>
+  </ul>`,
   providers: [HttpService],
   /*templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']*/
 })
 export class UserComponent implements OnInit {
 
-  user: User;
+  users: User[] = [];
   constructor(private http: HttpClient, private httpService: HttpService){}
-  ngOnInit(): void{ /*this.httpService.getData().subscribe((data: any) => this.users = data[  " userList " ]);*/
-   this.httpService.getData().subscribe((data: any) => this.user = new User(data.name, data.age));
+  ngOnInit(): void{
+    this.httpService.getUser().subscribe(res => { this.users = res; });
+    /*this.httpService.getData().subscribe((data: any) => this.users = data[  " userList " ]);*/
+  // this.httpService.getData().subscribe((data: any) => this.user = new User(data.name, data.age));
    /* this.http.get('assets/package.json').subscribe((data: any) => this.user = new User(data.name, data.age));*/
 }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class HttpService{
@@ -9,4 +10,16 @@ export class HttpService{
   getData(){
     return this.http.get('https://localhost:5001/api/Users/1');  /* ('assets/tsconfig.json');*/ /* package */
   }
+ public getUser(): Observable<User[]>
+  {
+    return this.http.get<User[]>('https://localhost:5001/api/Users');
+  }
+}
+
+
+
+export interface User{
+  id: number;
+  name: string;
+  age: number;
 }
